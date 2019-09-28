c9129db11bba539f250cb4c927ca47f6cb65e3a9
Gaurav Agarwal <gaurav.a.agarwal@capgemini.com>
2019-09-28 12:58:28 +0100

    Initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 monitor()
 {
+    /usr/bin/ssh-keygen -A
+	/usr/sbin/sshd
+	netstat -tnlp
 	echo "----------------------------------------"
 	echo "Running - stop container to exit"
 	# Loop forever by default - container must be stopped manually.
